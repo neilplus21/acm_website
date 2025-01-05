844fb09a60c6d3138a4e884c9bba235945dfc194
kushagr <skushagr15@gmail.com>
2025-01-05 21:32:07 +0530

    google anayltics added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import {
   createBrowserRouter,
   RouterProvider,
+  useNavigationType,
+  useLocation
 } from "react-router-dom";
+import ReactGA from 'react-ga';
 import Pages from '../src/Pages';
 import Members from './components/Members/Members';
-import Navbar from './components/Navbar/Navbar';
 import TerminalAnimation from './TerminalAnimation';
 
+const TRACKING_ID = 'G-WDP405HRFR'; // Replace with your actual GA4 Measurement ID
+ReactGA.initialize(TRACKING_ID);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Pages />,
+  },
+  {
+    path: "/members",
+    element: <Members />,
+  },
+  {
+    path: "/t",
+    element: <TerminalAnimation />
+  }
+]);
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Pages />,
-    },
-    {
-      path: "/members",
-      element: <Members />,
-    },
-
-    {
-      path: "/t",
-      element: <TerminalAnimation />
-    }
-
-    
-  ]);
-
   return (
     <div className="App">
-      <RouterProvider router={router} />
-
+      {/* RouterProvider wraps AnalyticsWrapper */}
+      <RouterProvider router={router}>
+        <AnalyticsWrapper />
+      </RouterProvider>
     </div>
   );
+}
+
+// Component to track page views
+function AnalyticsWrapper() {
+  const navigationType = useNavigationType();
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location, navigationType]);
+
+  return null; // No need to render anything
 }
 
 export default App;
